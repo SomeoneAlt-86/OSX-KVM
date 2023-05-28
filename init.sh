06d3920ccba0ef1512917ba24aa5422c083e7a7d
SomeoneAlt-86 <76504556+SomeoneAlt-86@users.noreply.github.com>
2023-05-28 10:56:29 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
+echo Checking Requirements 
+sudo apt-get install qemu uml-utilities virt-manager git \
+    wget libguestfs-tools p7zip-full make dmg2img -y > /dev/null 2>&1
+echo
+echo Setting up KVM-Qemu
+echo 1 | sudo tee /sys/module/kvm/parameters/ignore_msrs 
+sudo usermod -aG kvm $(whoami)
+sudo usermod -aG libvirt $(whoami)
+sudo usermod -aG input $(whoami)    
+echo Done!
+echo
 
+echo Preparing to download OSX
 python3 fetch-macOS-v2.py
-dmg2img -i BaseSystem.dmg BaseSystem.img
+echo
+echo Preparing Disks and Images
+dmg2img -i BaseSystem.dmg BaseSystem.img > /dev/null 2>&1
+qemu-img create -f qcow2 mac_hdd_ng.img 128G > /dev/null 2>&1
+echo All Done!
+echo
+echo Starting..
 ./OpenCore-Boot.sh
